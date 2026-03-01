0afc7ad5d665f3a072b228eb87479ce74e102da2
Ayush Rathi <rathiaccess@gmail.com>
2026-03-01 16:30:06 +0530

    Add project files.

---
+++ Day7_JavaScript_Event/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using System.Diagnostics;
+using Day7_JavaScript_Event.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Day7_JavaScript_Event.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+
+
+        public IActionResult Homee()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
